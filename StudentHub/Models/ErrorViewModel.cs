20b4ce34ef82fb3a8971773b3029931327163b6c
QuocBry-Dev <nguyenwuoc2004.dev@gmail.com>
2026-06-18 23:40:37 +0700

    Source code

---
+++ StudentHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
