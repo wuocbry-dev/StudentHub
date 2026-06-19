d2a16ec20c62725588752ebb2da1343eebf68fec
Twoan <truongduongtrongtoan2020@gmail.com>
2026-06-19 20:27:19 +0700

    fix: apply pending database migrations on startup

--- StudentHub/Program.cs
+++ StudentHub/Program.cs
@@ -31,6 +31,12 @@ namespace StudentHub
 
             var app = builder.Build();
 
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<SimsDbContext>();
+                dbContext.Database.Migrate();
+            }
+
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
